3edd331bdba55fc4563330e4a417d8c8300df1f8
Ryan Hoang <ryanhoang38@gmail.com>
2023-01-25 21:54:07 -0600

    db configurations

--- app/settings.py
+++ app/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -76,19 +77,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 
 # default database configurations
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'skilltreedb',
         'USER': 'root',
-        'PASSWORD': 'We#1envision',
+        'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
