d3b74e8af5c2a81350fa3614b58e46132fd85a69
Ryan Hoang <55223126+ryanhoang10@users.noreply.github.com>
2023-01-26 12:36:13 -0600

    Start of simple rest api

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+# Returns an element for inclusion in urlpatterns. 
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
--- api/views.py
+++ api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+# provides a nicer interface for returning content-negotiated Web API responses, that can be rendered to multiple formats.
+from rest_framework.response import Response
+#  takes a list of HTTP methods that your view should respond to.
+from rest_framework.decorators import api_view
+
+# example
+# @api_view(['GET', 'POST'])
+# def hello_world(request):
+#     if request.method == 'POST':
+#         return Response({"message": "Got some data!", "data": request.data})
+#     return Response({"message": "Hello, world!"})
+
+
+@api_view(['GET'])
+def getData(request):
+    person = {'name':'Ryan', 'age':25} 
+    return Response(person)
