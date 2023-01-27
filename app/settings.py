8ed9f0eff5769dfa076b848617745e571ec2de34
Ryan Hoang <55223126+ryanhoang10@users.noreply.github.com>
2023-01-27 16:03:37 -0600

    Remove venv and include it into gitignore

--- app/settings.py
+++ app/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'skilltreedb',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': 'password',
         'HOST': '127.0.0.1',
         'PORT': '3306',
         'OPTIONS': {
