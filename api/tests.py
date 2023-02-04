c980599fec93a5dcbf13fc24ab7f5a1c0382addc
Ryan Hoang <ryanhoang38@gmail.com>
2023-02-03 23:13:39 -0600

    create basic rest framework api test

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,10 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase, APIRequestFactory, TestCase
+from .views import skillTreeDetails
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+
+class SkillTreeTestCase(APITestCase):
+    def test_skill_tree_list(self):
+        response = self.client.get('/skilltrees')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
