c74bdd3a3c95c9c4c0bbf6be8d69cc37a86b68a0
Ryan Hoang <ryanhoang38@gmail.com>
2023-02-03 20:13:48 -0600

    just keep it the same

--- api/urls.py
+++ api/urls.py
@@ -4,10 +4,10 @@ from . import views
 
 urlpatterns = [
     path('skilltrees', views.skillsTreeList),
-    path('skilltree/<int:pk>/', views.skillTreeDetails),
+    path('skilltrees/<int:pk>/', views.skillTreeDetails),
     path('users', views.usersList),
-    path('user/<int:pk>/', views.userDetails),
-    path('comment/<int:pk>/', views.commentDetails),
+    path('users/<int:pk>/', views.userDetails),
+    path('comments/<int:pk>/', views.commentDetails),
     path('likes/<int:pk>/', views.likeDetails),
     path('dislikes/<int:pk>/', views.dislikeDetails),
     path('skill_trees_nodes/<int:pk>/', views.skillTreeNodeDetails),
